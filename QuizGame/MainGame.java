6061f7c7f4ab0ecabff68792854965f15b4f4bae
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-05-15 07:27:05 +0530

    chore: Update QuizDAO creating methods for get the questions and answers from data base

--- QuizGame/MainGame.java
+++ QuizGame/MainGame.java
@@ -1,23 +1,14 @@
 package QuizGame;
 
-import java.util.Scanner;
-
 public class MainGame {
-    public static Scanner scanner=new Scanner(System.in);
     public static void main(String[]args){
         printMain();
-
         allQuestion();
     }
     public static void printMain(){
         System.out.println("=========================================");
         System.out.println("         WELCOME JAVA QUIZ GAME");
         System.out.println("=========================================");
-        System.out.print("Enter your name: ");
-        String name=scanner.nextLine();
-        System.out.println();
-        System.out.println("Hello, "+name+"! Let's begin the quiz.");
-        System.out.println("---------------------------------------");
     }
     public static void allQuestion(){
         QuizDAO.getQuestions();
